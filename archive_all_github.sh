f750b0e4c624ea9e2328b38275f856f621a4a6a5
Florent Langlois <langlois@synchrotron-soleil.fr>
2014-03-18 16:07:56 +0100

    - set HEAD instead of master

--- archive_all_github.sh
+++ archive_all_github.sh
@@ -1,68 +1,68 @@
 mkdir $dir
-echo -------------- Lima Global -------------------
-git archive master | tar -x -C $dir/
+echo -------------- Lima Core -------------------
+git archive HEAD | tar -x -C $dir/
 echo ----------------------------------------------
 echo
 
 echo -------------- ADSC --------------------------
 cd camera/adsc
-git archive master | tar -x -C $dir/camera/adsc/
+git archive HEAD | tar -x -C $dir/camera/adsc/
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Basler ------------------------
 cd camera/basler
-git archive master | tar -x -C $dir/camera/basler
+git archive HEAD | tar -x -C $dir/camera/basler
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- MarCCD ------------------------
 cd camera/marccd
-git archive master | tar -x -C $dir/camera/marccd/
+git archive HEAD | tar -x -C $dir/camera/marccd/
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- PCO ------------------------
 cd camera/pco
-git archive master | tar -x -C $dir/camera/pco/
+git archive HEAD | tar -x -C $dir/camera/pco/
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- perkinelmer ------------------------
 cd camera/perkinelmer
-git archive master | tar -x -C $dir/camera/perkinelmer/
+git archive HEAD | tar -x -C $dir/camera/perkinelmer/
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Pilatus -----------------------
 cd camera/pilatus
-git archive master | tar -x -C $dir/camera/pilatus/
+git archive HEAD | tar -x -C $dir/camera/pilatus/
 cd ../..
 echo ----------------------------------------------
 echo 
 
 echo -------------- Prosilica ---------------------
 cd camera/prosilica
-git archive master | tar -x -C $dir/camera/prosilica/
+git archive HEAD | tar -x -C $dir/camera/prosilica/
 cd ../..
 echo ----------------------------------------------
 echo 
 
 echo -------------- roperscientific ------------------------
 cd camera/roperscientific
-git archive master | tar -x -C $dir/camera/roperscientific/
+git archive HEAD | tar -x -C $dir/camera/roperscientific/
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Xpad --------------------------
 cd camera/xpad
-git archive master | tar -x -C $dir/camera/xpad/
+git archive HEAD | tar -x -C $dir/camera/xpad/
 cd ../..
 echo ----------------------------------------------
 echo
@@ -70,7 +70,7 @@ echo
 
 echo -------------- Tango -------------------------
 cd applications/tango
-git archive master | tar -x -C $dir/applications/tango/
+git archive HEAD | tar -x -C $dir/applications/tango/
 cd ../..
 echo ----------------------------------------------
 echo
@@ -78,7 +78,7 @@ echo
 
 echo -------------- Processlib --------------------
 cd third-party/Processlib
-git archive master | tar -x -C $dir/third-party/Processlib/
+git archive HEAD | tar -x -C $dir/third-party/Processlib/
 cd ../..
 echo ----------------------------------------------
 echo
