eb78a79d6eec68ebd4b35910788ff2ce745c05c3
Arafat Noureddine <noureddine@synchrotron-soleil.fr>
2014-07-30 17:16:06 +0200

    Add a new parameters in main arguments

--- TestLima/src/main.cpp
+++ TestLima/src/main.cpp
@@ -45,7 +45,7 @@ void wait_while_status_is_standby(CtControl& ct, Interface& hw)
 				{
 					cout << "Waiting for Request ..." << endl;
 					is_already_displayed = true;
-				}				
+				}
 			}
 				break;
 
@@ -138,10 +138,10 @@ void make_snap(CtControl& ct, Interface& hw)
 	//- start acqusition
 	cout << "startAcq" << endl;
 	ct.startAcq();
-	
+
 	//- waiting while state is standby ...	
 	wait_while_status_is_standby(ct, hw);
-	
+
 	clock_t start = clock();
 	//- waiting while state is running ...
 	wait_while_status_is_running(ct, hw);
@@ -149,7 +149,7 @@ void make_snap(CtControl& ct, Interface& hw)
 	clock_t end = clock();
 	double millis = (end - start) / 1000;
 	cout << "Elapsed time  = " << millis << " (ms)" << endl;
-		cout << "============================================\n" << endl;		
+	cout << "============================================\n" << endl;
 }
 
 //--------------------------------------------------------------------------------------
@@ -157,23 +157,30 @@ void make_snap(CtControl& ct, Interface& hw)
 //- 1st argument is the IP adress of camera
 //- 2nd argument is the exposure_time (in milli seconds)
 //- 3rd argument is the latency_time (in milli seconds)
-//- 4th argument is the nb. of snap to do
-//- 5th argument is the packet size of the socket transport layer
-//- 6th argument is the interpacket delay of the socket transport layer
+//- 4rd argument is the latency_time (in milli seconds)
+//- 5th argument is the nb. of frames to acquire
+//- 6th argument is the nb. of snap to do
+//- 7th argument is the Bpp
+//- 8th argument is the packet size of the socket transport layer
+//- 9th argument is the interpacket delay of the socket transport layer
 //--------------------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
+	cout << "============================================" << endl;
+	cout << "Usage :./ds_TestLima ip exp lat expacc nbframes nbsnap bpp packet interpacket" << endl << endl;
+
 	try
 	{
-		string ip_adress = "172.16.3.137";  //default value
-		double exposure_time = 500;      //default value is 1000 ms
-		double acc_max_exposure_time = 100;
-		double latency_time  = 0.0;         //default value is 0 ms
-		int nb_snap = 1;                   //default value
-		int packet_size = 1500;             //default value
-		int interpacket_delay = 0;          //default value        
-		int nb_frames = 10;
+		string ip_adress				= "172.16.3.137";	//default value
+		double exposure_time			= 500;				//default value is 1000 ms
+		double latency_time				= 0.0;				//default value is 0 ms
+		double acc_max_exposure_time	= 100;				//default value is 100 ms
+		int nb_frames					= 10;				//default value is 10
+		int nb_snap						= 1;                //default value
+		unsigned bpp					= 8;
+		int packet_size					= 1500;             //default value
+		int interpacket_delay			= 0;				//default value        
 
 		//read args of main 
 		switch (argc)
@@ -215,8 +222,8 @@ int main(int argc, char *argv[])
 				istringstream argLatency(argv[3]);
 				argLatency >> latency_time;
 
-				istringstream argNbSnap(argv[4]);
-				argNbSnap >> nb_snap;
+				istringstream argAccExposure(argv[4]);
+				argAccExposure >> acc_max_exposure_time;
 			}
 				break;
 
@@ -230,11 +237,11 @@ int main(int argc, char *argv[])
 				istringstream argLatency(argv[3]);
 				argLatency >> latency_time;
 
-				istringstream argNbSnap(argv[4]);
-				argNbSnap >> nb_snap;
+				istringstream argAccExposure(argv[4]);
+				argAccExposure >> acc_max_exposure_time;
 
-				istringstream argPacketSize(argv[5]);
-				argPacketSize >> packet_size;
+				istringstream argNbSnap(argv[5]);
+				argNbSnap >> nb_snap;
 			}
 				break;
 
@@ -248,27 +255,111 @@ int main(int argc, char *argv[])
 				istringstream argLatency(argv[3]);
 				argLatency >> latency_time;
 
-				istringstream argNbSnap(argv[4]);
+				istringstream argAccExposure(argv[4]);
+				argAccExposure >> acc_max_exposure_time;
+
+				istringstream argNbFrames(argv[5]);
+				argNbFrames >> nb_frames;
+
+				istringstream argNbSnap(argv[6]);
 				argNbSnap >> nb_snap;
-
-				istringstream argPacketSize(argv[5]);
-				argPacketSize >> packet_size;
-
-				istringstream argInterPacketDelay(argv[6]);
-				argInterPacketDelay >> interpacket_delay;
 
 			}
 				break;
 
+			case 8:
+			{
+				ip_adress = argv[1];
+
+				istringstream argExposure(argv[2]);
+				argExposure >> exposure_time;
+
+				istringstream argLatency(argv[3]);
+				argLatency >> latency_time;
+
+				istringstream argAccExposure(argv[4]);
+				argAccExposure >> acc_max_exposure_time;
+
+				istringstream argNbFrames(argv[5]);
+				argNbFrames >> nb_frames;
+
+				istringstream argNbSnap(argv[6]);
+				argNbSnap >> nb_snap;
+
+				istringstream argBpp(argv[7]);
+				argBpp >> bpp;
+
+			}
+				break;
+
+			case 9:
+			{
+				ip_adress = argv[1];
+
+				istringstream argExposure(argv[2]);
+				argExposure >> exposure_time;
+
+				istringstream argLatency(argv[3]);
+				argLatency >> latency_time;
+
+				istringstream argAccExposure(argv[4]);
+				argAccExposure >> acc_max_exposure_time;
+
+				istringstream argNbFrames(argv[5]);
+				argNbFrames >> nb_frames;
+
+				istringstream argNbSnap(argv[6]);
+				argNbSnap >> nb_snap;
+
+				istringstream argBpp(argv[7]);
+				argBpp >> bpp;
+
+				istringstream argPacketSize(argv[8]);
+				argPacketSize >> packet_size;
+			}
+				break;
+
+			case 10:
+			{
+				ip_adress = argv[1];
+
+				istringstream argExposure(argv[2]);
+				argExposure >> exposure_time;
+
+				istringstream argLatency(argv[3]);
+				argLatency >> latency_time;
+
+				istringstream argAccExposure(argv[4]);
+				argAccExposure >> acc_max_exposure_time;
+
+				istringstream argNbFrames(argv[5]);
+				argNbFrames >> nb_frames;
+
+				istringstream argNbSnap(argv[6]);
+				argNbSnap >> nb_snap;
+
+				istringstream argBpp(argv[7]);
+				argBpp >> bpp;
+
+				istringstream argPacketSize(argv[8]);
+				argPacketSize >> packet_size;
+
+				istringstream argInterPacketDelay(argv[9]);
+				argInterPacketDelay >> interpacket_delay;
+
+			}
+				break;
 		}
 
 		//        
 		cout << "============================================" << endl;
 		cout << "ip_adress              = " << ip_adress << endl;
 		cout << "exposure_time          = " << exposure_time << endl;
-		cout << "acc_max_exposure_time  = " << acc_max_exposure_time << endl;
 		cout << "latency_time           = " << latency_time << endl;
+		cout << "acc_max_exposure_time  = " << acc_max_exposure_time << endl;
+		cout << "nb_frames              = " << nb_frames << endl;
 		cout << "nb_snap                = " << nb_snap << endl;
+		cout << "bpp                    = " << bpp << endl;
 		cout << "packet_size            = " << packet_size << endl;
 		cout << "interpacket_delay      = " << interpacket_delay << endl;
 		cout << "============================================" << endl;
@@ -283,13 +374,40 @@ int main(int argc, char *argv[])
 		cout << "Create CtControl Object" << endl;
 		CtControl myControl(&myInterface);
 
-		cout << "============================================" << endl;		
+		cout << "============================================" << endl;
 		HwDetInfoCtrlObj *hw_det_info;
 		myInterface.getHwCtrlObj(hw_det_info);
 
-		//- fix imageType = Bpp16
-		cout << "Fix imageType \t= Bpp16" << endl;
-		hw_det_info->setCurrImageType(Bpp16);
+		switch (bpp)
+		{
+			case 8:
+				//- fix imageType = Bpp16
+				cout << "Fix imageType \t= " << bpp << endl;
+				hw_det_info->setCurrImageType(Bpp8);
+				break;
+
+			case 12:
+				//- fix imageType = Bpp16
+				cout << "Fix imageType \t= " << bpp << endl;
+				hw_det_info->setCurrImageType(Bpp12);
+				break;
+
+			case 16:
+				//- fix imageType = Bpp16
+				cout << "Fix imageType \t= " << bpp << endl;
+				hw_det_info->setCurrImageType(Bpp16);
+				break;
+			case 32:
+				//- fix imageType = Bpp16
+				cout << "Fix imageType \t= " << bpp << endl;
+				hw_det_info->setCurrImageType(Bpp32);
+				break;
+
+			default:
+				//- Error
+				cout << "Don't Fix imageType !" << endl;
+				break;				
+		}
 
 		myCamera.setInterPacketDelay(interpacket_delay);
 
@@ -297,7 +415,7 @@ int main(int argc, char *argv[])
 		Size size;
 		hw_det_info->getMaxImageSize(size);
 		Roi myRoi(0, 0, size.getWidth(), size.getHeight());
-		cout << "Fix Roi \t= ("<<0<<","<<0<<","<<size.getWidth()<<","<<size.getHeight()<<")" << endl;
+		cout << "Fix Roi \t= (" << 0 << "," << 0 << "," << size.getWidth() << "," << size.getHeight() << ")" << endl;
 		myControl.image()->setRoi(myRoi);
 
 		//- fix Bin = (1,1)
@@ -309,7 +427,7 @@ int main(int argc, char *argv[])
 		cout << "Fix exposure \t= " << exposure_time << " (ms)" << endl;
 		myControl.acquisition()->setAcqExpoTime(exposure_time / 1000.0); //convert exposure_time to sec
 		myControl.video()->setExposure(exposure_time / 1000.0); //convert exposure_time to sec
-		cout << "Fix Acc exposure= " << acc_max_exposure_time << " (ms)" << endl;		
+		cout << "Fix Acc exposure= " << acc_max_exposure_time << " (ms)" << endl;
 		myControl.acquisition()->setAccMaxExpoTime(acc_max_exposure_time / 1000.0); //convert exposure_time to sec
 
 		//- fix latency
@@ -317,7 +435,7 @@ int main(int argc, char *argv[])
 		myControl.acquisition()->setLatencyTime(latency_time / 1000.0);  //convert latency_time to sec
 
 		//- fix nbFrames = 1
-		cout << "Fix nbFrames \t= " << nb_frames<<endl;
+		cout << "Fix nbFrames \t= " << nb_frames << endl;
 		myControl.acquisition()->setAcqNbFrames(nb_frames);
 
 		cout << "\n" << endl;
