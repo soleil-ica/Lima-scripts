c6d56ff8a13634c77aa045704509a77816a17bbb
Arafat Noureddine <noureddine@synchrotron-soleil.fr>
2014-12-11 11:27:20 +0100

    - Enhance timing display - Increment the  exposure of each loop

--- test_limadetector_write_exposure_in_video.py
+++ test_limadetector_write_exposure_in_video.py
@@ -29,22 +29,22 @@ def run(proxy_name = 'arafat/lima_basler/basler.2', exposure_time = 100, write_p
     print 'nb_loops\t = ', nb_loops
     #Configure the device
     print '\nConfigure the Device :\n--------------'
-    timeBegin = datetime.datetime.now().isoformat()
-    print '\r',timeBegin, " - Create a DeviceProxy to Device ", proxy_name
+    timeBegin = datetime.datetime.now()
+    print '\r',timeBegin.isoformat(), " - Create a DeviceProxy to Device ", proxy_name
     proxy = PyTango.DeviceProxy(proxy_name)
-    timeBegin = datetime.datetime.now().isoformat()
-    print '\r',timeBegin, " - Ping proxy(\"",proxy_name,"\") ",proxy.ping()," us"
+    timeBegin = datetime.datetime.now()
+    print '\r',timeBegin.isoformat(), " - Ping proxy(\"",proxy_name,"\") ",proxy.ping()," us"
     
-    timeBegin = datetime.datetime.now().isoformat()
-    print '\r',timeBegin, " - State = ",proxy.state();
+    timeBegin = datetime.datetime.now()
+    print '\r',timeBegin.isoformat(), " - State = ",proxy.state();
     print '--> Stop()'
     proxy.Stop()
-    timeBegin = datetime.datetime.now().isoformat()
-    print '\r',timeBegin, " - State = ",proxy.state();
+    timeBegin = datetime.datetime.now()
+    print '\r',timeBegin.isoformat(), " - State = ",proxy.state();
     print '--> Start()'
     proxy.Start()
-    timeBegin = datetime.datetime.now().isoformat()
-    print '\r',timeBegin, " - State = ",proxy.state();
+    timeBegin = datetime.datetime.now()
+    print '\r',timeBegin.isoformat(), " - State = ",proxy.state();
     nb_loops=int(nb_loops)
     try:
         current_loop = 0
@@ -62,9 +62,16 @@ def run(proxy_name = 'arafat/lima_basler/basler.2', exposure_time = 100, write_p
 	   sys.stderr.write('--------------\nERROR :\n--------------\n%s\n' %err)
 
 #------------------------------------------------------------------------------
+# Usage
+#------------------------------------------------------------------------------
+def usage():
+  print "Usage: [python] test_limadetector_write_exposure_in_video.py <my/device/proxy> <exposureTime in ms> <writingPeriod in ms> <nb_loops>"
+  sys.exit(1)
+  
+#------------------------------------------------------------------------------
 # Main Entry point
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
-#    if len(sys.argv) < 4:
-#        usage()
+    if len(sys.argv) != 5:
+        usage()
     print run(*sys.argv[1:])
