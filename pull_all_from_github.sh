b821bcd1c16af1d02584468471d5899f5a48abac
langlois <langlois@synchrotron-soleil.fr>
2015-07-09 15:34:44 +0200

    modif with --rebase

--- pull_all_from_github.sh
+++ pull_all_from_github.sh
@@ -1,88 +1,93 @@
+#!/bin/sh
+
+# Author: FL / AN
+# Date: 12/12/2014
+
 echo -------------- Lima Global -------------------
-git pull git@github.com:soleil-ica/Lima.git
+git pull --rebase git@github.com:soleil-ica/Lima.git
 echo ----------------------------------------------
 echo
 
 echo -------------- ADSC --------------------------
 cd camera/adsc
-git pull git@github.com:soleil-ica/Lima-camera-adsc
+git pull --rebase git@github.com:soleil-ica/Lima-camera-adsc
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Andor --------------------------
 cd camera/andor
-git pull git@github.com:soleil-ica/Lima-camera-andor
+git pull --rebase git@github.com:soleil-ica/Lima-camera-andor
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Aviex --------------------------
 cd camera/aviex
-git pull git@github.com:soleil-ica/Lima-camera-aviex
+git pull --rebase git@github.com:soleil-ica/Lima-camera-aviex
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Basler ------------------------
 cd camera/basler
-git pull git@github.com:soleil-ica/Lima-camera-basler
+git pull --rebase git@github.com:soleil-ica/Lima-camera-basler
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Eiger ------------------------
 cd camera/eiger
-git pull git@github.com:soleil-ica/Lima-camera-eiger
+git pull --rebase git@github.com:soleil-ica/Lima-camera-eiger
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- MarCCD ------------------------
 cd camera/marccd
-git pull git@github.com:soleil-ica/Lima-camera-marccd
+git pull --rebase git@github.com:soleil-ica/Lima-camera-marccd
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Merlin ------------------------
 cd camera/merlin
-git pull git@github.com:soleil-ica/Lima-camera-merlin
+git pull --rebase git@github.com:soleil-ica/Lima-camera-merlin
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- PCO ------------------------
 cd camera/pco
-git pull git@github.com:soleil-ica/Lima-camera-pco
+git pull --rebase git@github.com:soleil-ica/Lima-camera-pco
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Perkin Elmer ------------------------
 cd camera/perkinelmer
-git pull git@github.com:soleil-ica/Lima-camera-perkinelmer
+git pull --rebase git@github.com:soleil-ica/Lima-camera-perkinelmer
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Prosilica -----------------------
 cd camera/prosilica
-git pull git@github.com:soleil-ica/Lima-camera-prosilica
+git pull --rebase git@github.com:soleil-ica/Lima-camera-prosilica
 cd ../..
 echo ----------------------------------------------
 echo 
 
 echo -------------- Roperscientific -----------------------
 cd camera/roperscientific
-git pull git@github.com:soleil-ica/Lima-camera-roperscientific
+git pull --rebase git@github.com:soleil-ica/Lima-camera-roperscientific
 cd ../..
 echo ----------------------------------------------
 echo 
 
 echo -------------- Xpad --------------------------
 cd camera/xpad
-git pull git@github.com:soleil-ica/Lima-camera-xpad
+git pull --rebase git@github.com:soleil-ica/Lima-camera-xpad
 cd ../..
 echo ----------------------------------------------
 echo
@@ -90,7 +95,7 @@ echo
 
 echo -------------- Tango -------------------------
 cd applications/tango
-git pull git@github.com:soleil-ica/Lima-tango
+git pull --rebase git@github.com:soleil-ica/Lima-tango
 cd ../..
 echo ----------------------------------------------
 echo
@@ -98,7 +103,7 @@ echo
 
 echo -------------- Processlib --------------------
 cd third-party/Processlib
-git pull git@github.com:soleil-ica/Processlib.git
+git pull --rebase git@github.com:soleil-ica/Processlib.git
 cd ../..
 echo ----------------------------------------------
 echo
