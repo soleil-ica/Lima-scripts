75bcbcc105c9aaae669435f469c98de042790e8f
langlois <langlois@synchrotron-soleil.fr>
2018-02-22 10:15:53 +0100

    fix pull command

--- pull_all_from_origin.sh
+++ pull_all_from_origin.sh
@@ -4,127 +4,125 @@
 # Date: 12/12/2014
 
 echo -------------- Lima Global -------------------
-git pull --rebase git@github.com:soleil-ica/Lima.git
+git pull --rebase
 echo ----------------------------------------------
 echo
 
 echo -------------- Andor --------------------------
 cd camera/andor
-git pull --rebase git@github.com:soleil-ica/Lima-camera-andor
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Aviex --------------------------
 cd camera/aviex
-git pull --rebase git@github.com:soleil-ica/Lima-camera-aviex
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Basler ------------------------
 cd camera/basler
-git pull --rebase git@github.com:soleil-ica/Lima-camera-basler
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Eiger ------------------------
 cd camera/eiger
-git pull --rebase git@github.com:soleil-ica/Lima-camera-eiger
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Hamamatsu --------------------------
 cd camera/hamamatsu
-git pull --rebase git@github.com:soleil-ica/Lima-camera-hamamatsu
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- imXpad --------------------------
 cd camera/imxpad
-git pull --rebase git@github.com:soleil-ica/Lima-camera-imxpad
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- MarCCD ------------------------
 cd camera/marccd
-git pull --rebase git@github.com:soleil-ica/Lima-camera-marccd
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Merlin ------------------------
 cd camera/merlin
-git pull --rebase git@github.com:soleil-ica/Lima-camera-merlin
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- PCO ------------------------
 cd camera/pco
-git pull --rebase git@github.com:soleil-ica/Lima-camera-pco
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Perkin Elmer ------------------------
 cd camera/perkinelmer
-git pull --rebase git@github.com:soleil-ica/Lima-camera-perkinelmer
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Pilatus -----------------------
 cd camera/pilatus
-git pull --rebase git@github.com:soleil-ica/Lima-camera-pilatus PilatusSoleil
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo 
 
 echo -------------- Prosilica -----------------------
 cd camera/prosilica
-git pull --rebase git@github.com:soleil-ica/Lima-camera-prosilica
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo 
 
 echo -------------- Roperscientific -----------------------
 cd camera/roperscientific
-git pull --rebase git@github.com:soleil-ica/Lima-camera-roperscientific
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo 
 
 echo -------------- Uview --------------------------
 cd camera/uview
-git pull --rebase git@github.com:soleil-ica/Lima-camera-uview
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
 echo -------------- Xpad --------------------------
 cd camera/xpad
-git pull --rebase git@github.com:soleil-ica/Lima-camera-xpad
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
 
-
 echo -------------- Tango/cpp -------------------------
 cd applications/tango/cpp
-git pull --rebase git@github.com:soleil-ica/Lima-tango-cpp
+git pull --rebase
 cd ../../..
 echo ----------------------------------------------
 echo
 
-
 echo -------------- Processlib --------------------
 cd third-party/Processlib
-git pull --rebase git@github.com:soleil-ica/Processlib.git
+git pull --rebase
 cd ../..
 echo ----------------------------------------------
 echo
